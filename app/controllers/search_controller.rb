b671ada5b6276a889c45e68395541ca2a058a67a
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-10-30 10:36:16 -0600

    Started refactoring too early, went back and got test passing. Now refactoring

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def show
-    word = params[:p]
+    @word = params[:p]
     conn = Faraday.new( url: "https://od-api.oxforddictionaries.com/api/v1") do |faraday|
       faraday.headers["app_id"] = ENV["app_id"]
       faraday.headers["app_key"] = ENV["app_key"]
@@ -8,13 +8,12 @@ class SearchController < ApplicationController
       faraday.adapter Faraday.default_adapter
     end
 
-    response = conn.get("entries/en/#{word}/sentences")
+    response = conn.get("entries/en/#{@word}/sentences")
     result = JSON.parse(response.body)
     @sentences = result["results"].first["lexicalEntries"].first["sentences"].map do |sentence|
       if sentence["regions"].include?("British") || sentence["regions"].include?("Canadian")
         Sentence.new(sentence)
       end
     end.compact
-    binding.pry
   end
 end
--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,6 +1,7 @@
 class Sentence
+  attr_reader :region, :text
   def initialize(sentence_data)
-    @region = sentence_data["regions"]
+    @region = sentence_data["regions"].first
     @text = sentence_data["text"]
   end
 end
