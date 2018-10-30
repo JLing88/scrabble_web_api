41f1f33b30163e2d2195411d586565861c66cca2
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-10-30 09:56:19 -0600

    API call working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,16 @@
 class SearchController < ApplicationController
   def show
-    Faraday.new(
-      url: "https://od-api.oxforddictionaries.com/api/v1",
-      headers: {"app_id" = ENV["app_id"], "app_key" = ENV["app_key"], "Accept" = "application/json"}
-                ) do |faraday|
+    word = params[:p]
+    conn = Faraday.new( url: "https://od-api.oxforddictionaries.com/api/v1") do |faraday|
+      faraday.headers["app_id"] = ENV["app_id"]
+      faraday.headers["app_key"] = ENV["app_key"]
+      faraday.headers["Accept"] = "application/json"
       faraday.adapter Faraday.default_adapter
     end
+
+    response = conn.get("entries/en/#{word}")
+    result = JSON.parse(response.body)
+  
     @search = SearchFacade.new(params[:p])
   end
 end
